b84978b84307f71b9d0cb761cc21e96732917ce5
DHINESH KUMAR <dhineshsurya0829@gmail.com>
2024-03-04 12:08:40 +0530

    hi

---
+++ login_page/email-otp.js
@@ -0,0 +1,35 @@
+function sendOTP() {
+    const email = document.getElementById('email');
+    const otpverify = document.getElementsByClassName('otpverify')[0];
+
+    let otp_val = Math.floor(Math.random() * 10000);
+
+    let emailbody =`<h2>Your OTP is </h2>${otp_val}`;
+    Email.send({
+        SecureToken :"1083d43c-d6c4-4b71-bb1c-a4ce5abf392c",
+        To : email.value,
+        From : "dhineshsurya0829@gmail.com",
+        Subject : "email otp for verification",
+        Body : emailbody
+}).then(
+
+    message => {
+        if(message === "OK") {
+            alert("OTP sent to your email " + email.value);
+
+            otpverify.style.display = "flex";
+            const otp_inp = document.getElementById('otp_inp');
+            const otp_btn = document.getElementById('otp-btn');
+
+            otp_btn.addEventListener('click', () =>{
+                if (otp_inp.value == otp_val) {
+                    alert("Email address verified...");
+                }
+                else {
+                    alert("Invalid OTP");
+                }
+            })
+        }
+    }
+);
+}
